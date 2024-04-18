794246fc28d04d2c5477914bb4ef70aa7ea5f0b6
adsoft <adsoft@live.com.mx>
2024-04-17 19:45:57 -0600

    add fare graph

--- hola.py
+++ hola.py
@@ -1,6 +1,7 @@
 # importamos la biblioteca streamlit
 import streamlit as st 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # creamos el titulo de la App
 st.title("Titanic App")
@@ -10,3 +11,8 @@ st.write("Graficas del dataset titanic")
 titanic_link = 'titanic.csv'
 titanic_data = pd.read_csv(titanic_link)
 st.dataframe(titanic_data)
+
+fig, ax = plt.subplots()
+ax.hist(titanic_data["Fare"])
+st.header("Histograma del Titanic")
+st.pyplot(fig)
